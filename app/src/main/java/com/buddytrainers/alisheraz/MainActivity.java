9874faf0bde98e5666952ae89251925cf288f409
Ali Sheraz <alisheraz7777@gmail.com>
2020-02-01 09:26:54 -0800

    Initial commit

---
+++ app/src/main/java/com/buddytrainers/alisheraz/MainActivity.java
@@ -0,0 +1,30 @@
+package com.buddytrainers.alisheraz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void showCustomToast(View v)
+    {
+        View convertedXml;
+        LayoutInflater objectLayoutInflater=getLayoutInflater();
+        convertedXml=objectLayoutInflater.inflate( R.layout.ali_cutsom_toast,null );
+        Toast objectToast=new Toast(this);
+        objectToast.setGravity(Gravity.FILL_HORIZONTAL, 0, 0);
+        objectToast.setDuration(Toast.LENGTH_LONG);
+        objectToast.setView(convertedXml);
+        objectToast.show();
+    }
+}
